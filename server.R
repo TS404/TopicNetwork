b451b18edcf51199f7bbc55789c018af33194a64
Thomas Shafee <TS404@users.noreply.github.com>
2020-04-27 15:07:11 +1000

    interface updates

--- server.R
+++ server.R
@@ -7,37 +7,28 @@
 #    http://shiny.rstudio.com/
 #
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 #Setup --------
 #> Packages  -------------
 
-  library('WikipediR')
   library('WikidataR')
   library('WikidataQueryServiceR')
   library('tibble')
   library('devtools')
   library('stringr')
-  library('tidytext')
-  library('htmltidy')
   library('dplyr')
-  library('readr')
-  library('xml2')
-  library('lubridate')
   library('igraph')
   library('networkD3')
   library('dbscan')
   library('Hmisc')
-  
-  devtools::install_github('chainsawriot/pediarr')
-  devtools::install_github('mattflor/chorddiag')
-  devtools::install_github('garthtarr/edgebundleR')
-  devtools::install_github('jcheng5/bubbles')
-  library('pediarr')
   library('chorddiag')
-  library('edgebundleR')
-  library('bubbles')
+  # library('lubridate')#
+  # library('WikipediR')#
+  # library('tidytext')#
+  # library('htmltidy')#
+  # library('readr')#
+  # library('xml2')#
   
-
 #> Functions ------
 initials <- function(x,type="FLast"){
   if (type=="FLast"){
@@ -71,24 +62,29 @@ qid_from_name <- function(name   = 'Thomas Shafee',
 
 colMax <- function(x) {if(is.null(dim(x))){x}else{apply(x, 2, max)}}
 
-#> Calculations --------
+qdesc <- function(x){
+  y   <- WikidataR::find_item(x)
+  out <- paste0(y[[1]]$label,
+                ' (<a href="https://wikidata.org/wiki/',
+                y[[1]]$id,
+                '">',
+                y[[1]]$id,
+                '</a>) - ',
+                y[[1]]$description)
+  out
+}
+
+# Calculations --------
 
 input.button <- eventReactive(input$button.view,{
-  qid_from_name(switch(input$rb,
-                       example1 = c('Covid-19','SARS-CoV-2','2019–20 COVID-19 pandemic'),
-                       example2 = c('defensin', 'protease', 'cyclotide', 'cyclic peptide', 'protein'),
-                       custom   = (unlist(strsplit(input$query,",")))),
-                limit  = 1,
-                unlist = TRUE)
-})
-
-output$force  <- renderForceNetwork({ #moved up from vis to encompass input$query
-
-nucleation.Qs <- input.button()
-
-output$query  <- renderPrint({ paste(lapply(nucleation.Qs,qdesc),collapse = "\n") })
-
-sparql_query  <- paste0('SELECT ?node1 ?node2 ?node1Label ?node2Label ?pdate ?topicLabel ?authorLabel (COUNT(?work) AS ?count)
+  nucleation.Qs <- qid_from_name(switch(input$rb,
+                               example1 = c('Covid-19','SARS-CoV-2','2019–20 COVID-19 pandemic'),
+                               example2 = c('antiviral drug','defensin', 'protease', 'cyclic peptide'),
+                               custom   = (unlist(strsplit(input$query,",")))),
+                        limit  = 1,
+                        unlist = TRUE)
+  
+  sparql_query  <- paste0('SELECT ?node1 ?node2 ?node1Label ?node2Label ?pdate ?topicLabel ?authorLabel (COUNT(?work) AS ?count)
                          WHERE {
                            VALUES ?nucleation_topics { ',paste(paste0('wd:',nucleation.Qs),collapse = " "),' }
                            ?work wdt:P921 ?nucleation_topics, ?node1, ?node2. # Find co-nodes
@@ -100,89 +96,96 @@ sparql_query  <- paste0('SELECT ?node1 ?node2 ?node1Label ?node2Label ?pdate ?to
                          GROUP BY ?node1 ?node2 ?node1Label ?node2Label ?pdate ?topicLabel ?authorLabel
                          ORDER BY DESC(?count)
                          LIMIT 10000')
-links.qr   <- suppressMessages(unspecial(query_wikidata(sparql_query)))
+  links.qr   <- suppressMessages(unspecial(query_wikidata(sparql_query)))
+  links.qr$node1Label <- ordered(links.qr$node1Label,         # define factor order by most linked
+                                 unique(names(sort(table(links.qr$node1Label),TRUE))))
+  links.qr   <- links.qr[with(links.qr, order(node1Label)),] # order all by that factor order
+  links.qr.r <- t(apply(links.qr[,3:4], 1, sort))                 # find reciprocal links and flip them
+  links.qr   <- links.qr[!duplicated(links.qr.r),]             # remove flipped reciprocal links (now show up as duplicates)
 
-links.qr$node1Label <- ordered(links.qr$node1Label,         # define factor order by most linked
-                               unique(names(sort(table(links.qr$node1Label),TRUE))))
-links.qr   <- links.qr[with(links.qr, order(node1Label)),] # order all by that factor order
-links.qr.r <- t(apply(links.qr[,3:4], 1, sort))                 # find reciprocal links and flip them
-links.qr   <- links.qr[!duplicated(links.qr.r),]             # remove flipped reciprocal links (now show up as duplicates)
-
-links.qr
-
-#> igraph --------------
-data <- links.qr[order(links.qr$count),]
-g    <- graph_from_edgelist(as.matrix(data[,3:4]),
-                            directed = 0) %>%
-  set_edge_attr("weight", value = data$count)
-clp  <- cluster_label_prop(g,weights = E(g)$weight)
-hcl  <- hclust(dist(g[]))
-hdb  <- dbscan::hdbscan(dist(g[]),minPts = 3)
-l    <- layout.graphopt(g,spring.length = E(g)$weight)
-
-groups           <- hdb$cluster
-groups           <- clp$membership
-max.groups       <- max(groups)
-colours          <- RColorBrewer::brewer.pal(max.groups,'Dark2')
-colours          <- viridis::plasma(max.groups)
-colours          <- colorRampPalette(c('#ee4400','#ff0099','#440055','#000055','#006688','#005533'))(max.groups)
-if(min(groups)==0){colours[1]<-"white"}
-barplot(rep(1,length(colours)),col=colours,space=0,border=if(length(colours)>20){NA},main="group colour palette used")
-group.order      <- order(groups,hdb$cluster)
-group.colours    <- colours[groups]
-group.colours[1:length(nucleation.Qs)] <- "black"
-
-
-#> D3 net ------------------
-
-links.D3 <- data.frame(node1Q     = links.qr$node1,
-                       node2Q     = links.qr$node2,
-                       node1Label = links.qr$node1Label,
-                       node2Label = links.qr$node2Label,
-                       count      = links.qr$count)
-
-node_names <- iconv(clp$names,to = 'ASCII//TRANSLIT')
-
-links.D3$node1Label <- ordered(iconv(links.D3$node1Label,to='ASCII//TRANSLIT'),node_names)
-links.D3$node2Label <- ordered(iconv(links.D3$node2Label,to='ASCII//TRANSLIT'),node_names)
-
-links.D3$node1Label <- as.numeric(links.D3$node1Label)-1 # zero-indexed
-links.D3$node2Label <- as.numeric(links.D3$node2Label)-1 # zero-indexed
-links.D3$count      <- as.numeric(links.D3$count)
-links.D3$colour     <- colorRampPalette(c(rgb(0,0,0,0.3),rgb(0,0,0,0.9)), alpha=TRUE)(max(E(g)$weight))[E(g)$weight]
-
-# node_sizes <- c(max(links.D3$count),links.D3$count[links.D3$node1Label==0|1|2][order(links.D3$node2Label[links.D3$node1Label==0])]) #old version
-if(length(nucleation.Qs)==1){
-  node_sizes <- g[unique(links.qr$node1Label[grep(paste(nucleation.Qs[1]),links.qr$node1)]),][node_names] 
-}else{
-  node_sizes <- colMax(as.matrix(g[unique(links.qr$node1Label[grep(paste(nucleation.Qs,collapse="|"),links.qr$node1)]),]))[node_names]
-}
-node_sizes[1:length(nucleation.Qs)] <- max(node_sizes)
-
-normalise <- 1
-if(normalise){
-  links.D3$count     <- 1+9*sqrt((links.qr$count-1)/max(links.qr$count-1))
-  node_sizes         <- 1+3*(log10(1+1000*(node_sizes-1)/max(node_sizes-1)))
-}
-
-nodes.D3 <- data.frame(name  = node_names,
-                       group = groups,
-                       size  = node_sizes)
-nodes.D3$group[1:length(nucleation.Qs)] <- 0
-
-MyClickScript <- 'alert("You clicked " + d.name + " which has size " + d.nodesize );'
-
-# Visualisations ---------
-
-#> renderForceNetwork ---------
-# output$force <- renderForceNetwork({
+  #> igraph --------------
+  links.qr.o <- links.qr[order(links.qr$count),]
+  g    <- graph_from_edgelist(as.matrix(links.qr.o[,3:4]),
+                              directed = 0) %>%
+          set_edge_attr("weight", value = links.qr.o$count)
+  clp  <- cluster_label_prop(g,weights = E(g)$weight)
+  hcl  <- hclust(dist(g[]))
+  hdb  <- dbscan::hdbscan(dist(g[]),minPts = 3)
+  l    <- layout.graphopt(g,spring.length = E(g)$weight)
+  
+  groups           <- hdb$cluster
+  groups           <- clp$membership
+  max.groups       <- max(groups)
+  colours          <- RColorBrewer::brewer.pal(max.groups,'Dark2')
+  colours          <- viridis::plasma(max.groups)
+  colours          <- colorRampPalette(c('#ee4400','#ff0099','#440055','#000055','#006688','#005533'))(max.groups)
+  if(min(groups)==0){colours[1]<-"white"}
+  barplot(rep(1,length(colours)),col=colours,space=0,border=if(length(colours)>20){NA},main="group colour palette used")
+  group.order      <- order(groups,hdb$cluster)
+  group.colours    <- colours[groups]
+  group.colours[1:length(nucleation.Qs)] <- "black"
+  group.colours    <- group.colours[group.order]
+  data.grouped     <- log10(1+100*as.matrix(get.adjacency(g,attr = "weight",sparse = T))[group.order,group.order]/max(g[]))
+  
+  #> D3 net ------------------
+  
+  links.D3 <- data.frame(node1Q     = links.qr$node1,
+                         node2Q     = links.qr$node2,
+                         node1Label = links.qr$node1Label,
+                         node2Label = links.qr$node2Label,
+                         count      = links.qr$count)
+  
+  node_names <- iconv(clp$names,to = 'ASCII//TRANSLIT')
+  
+  links.D3$node1Label <- ordered(iconv(links.D3$node1Label,to='ASCII//TRANSLIT'),node_names)
+  links.D3$node2Label <- ordered(iconv(links.D3$node2Label,to='ASCII//TRANSLIT'),node_names)
+  
+  links.D3$node1Label <- as.numeric(links.D3$node1Label)-1 # zero-indexed
+  links.D3$node2Label <- as.numeric(links.D3$node2Label)-1 # zero-indexed
+  links.D3$count      <- as.numeric(links.D3$count)
+  links.D3$colour     <- colorRampPalette(c(rgb(0,0,0,0.3),rgb(0,0,0,0.9)), alpha=TRUE)(max(E(g)$weight))[E(g)$weight]
+  
+  # node_sizes <- c(max(links.D3$count),links.D3$count[links.D3$node1Label==0|1|2][order(links.D3$node2Label[links.D3$node1Label==0])]) #old version
+  if(length(nucleation.Qs)==1){
+    node_sizes <- g[unique(links.qr$node1Label[grep(paste(nucleation.Qs[1]),links.qr$node1)]),][node_names] 
+  }else{
+    node_sizes <- colMax(as.matrix(g[unique(links.qr$node1Label[grep(paste(nucleation.Qs,collapse="|"),links.qr$node1)]),]))[node_names]
+  }
+  node_sizes[1:length(nucleation.Qs)] <- max(node_sizes)
+  
+  normalise <- 1
+  if(normalise){
+    links.D3$count     <- 1+9*sqrt((links.qr$count-1)/max(links.qr$count-1))
+    node_sizes         <- 1+3*(log10(1+1000*(node_sizes-1)/max(node_sizes-1)))
+  }
   
   nodes.D3 <- data.frame(name  = node_names,
                          group = groups,
                          size  = node_sizes)
+  nodes.D3$group[1:length(nucleation.Qs)] <- 0
+
+  list(nucleation.Qs = nucleation.Qs,
+       sparql_query  = sparql_query,
+       links.qr      = links.qr,
+       g             = g,
+       links.D3      = data.frame(links.D3),
+       nodes.D3      = data.frame(nodes.D3),
+       data.grouped  = data.grouped,
+       group.colours = group.colours
+       )
+})
+
+# Text -------------
+output$qids  <- renderUI({ HTML(paste(lapply(input.button()$nucleation.Qs,qdesc),collapse = "<br/>")) })
+
+# Visualisations ---------
+#> renderForceNetwork ---------
+output$force <- renderForceNetwork({
   
-  forceNetwork(Links        = data.frame(links.D3),
-               Nodes        = data.frame(nodes.D3),
+  forceNetwork(Links        = input.button()$links.D3,
+               Nodes        = input.button()$nodes.D3,
+               linkColour   = input.button()$links.D3$colour,
+               charge       = input$charge*-3,
                Source       = "node1Label",
                Target       = "node2Label",
                Value        = "count",
@@ -193,17 +196,27 @@ MyClickScript <- 'alert("You clicked " + d.name + " which has size " + d.nodesiz
                opacity      = 0.8,
                fontFamily   = "sans-serif",
                fontSize     = 20,
-               # width        = 1000,
                height       = 5000,
-               # bounded      = 1,
-               charge       = -3*input$charge,
-               linkColour   = links.D3$colour,
-               clickAction  = MyClickScript,
-               linkWidth    = JS("function(d){return 1+(d.value)}"),
-               linkDistance = JS("function(d){return 2/(1+d.value)}"),
+               clickAction  = 'alert("You clicked " + d.name);',
+               linkWidth    = JS('function(d){return 1+(d.value)}'),
+               linkDistance = JS('function(d){return 2/(1+d.value)}'),
                radiusCalculation = JS("(d.nodesize)+3")
   )
 })
+
+#> renderChordDiag ---------
+output$chord <- renderChorddiag({
+  chorddiag(data              = as.matrix(input.button()$data.grouped),
+            groupColors       = as.matrix(input.button()$group.colours),
+            groupPadding      = if(ncol(input.button()$g[])<100){1}else{0},
+            showTicks         = F,
+            margin            = 80,
+            groupnameFontsize = 12,
+            groupnamePadding  = 10,
+            chordedgeColor    = '#00000022',
+            groupedgeColor    = '#00000099')
+})
+
 
 }) # end of shinyServer
 
--- ui.R
+++ ui.R
@@ -9,50 +9,70 @@
 
 library(shiny)
 library(networkD3)
+library(chorddiag)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
+    tags$head(
+        tags$style(HTML("hr {border-top: 1px solid #999;}"))
+    ),
     # Application title
     titlePanel("Co-topic network from Wikidata"),
     wellPanel(
         div(class="header",
             p("This tool calculates a network of topics.
-               Each 'node' is a topic, and 'edges' are shown whenever a scientific publication is about both topics."),
-            p("The data is drawn from Wikidata by searching outwards from the starting topic(s).
-               If clear clusters can be identified, they'll be assigned group colours."),
-            p("The more publications that are about a pair of topics, the thicker the link between them.
-               Node size indicates topics that commly co-occur in publications  with the starting topic(s).")
+               Each 'node' is a topic, and 'edges' are shown whenever a scientific publication is about both topics.
+               The data is drawn from Wikidata by searching outwards from the starting topic(s).
+               Searches can take >30 seconds.
+               If clear clusters can be identified, they'll be assigned group colours.
+               The more publications that are about a pair of topics, the thicker the link between them.
+               Node size indicates topics that commly co-occur in publications  with the starting topic(s)."
+              )
             )
     ),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            textAreaInput("query",resize="horizontal",rows=1,
-                          label       = "Starting topic",
+            h4("Starting topic for network"),
+            conditionalPanel(
+                condition = "input.rb == 'custom'",
+                textInput("query",
+                          label       = NULL,
                           value       = "COVID-19",
                           placeholder = "Search term to start the graph"),
-            radioButtons("rb", "Choose one:",
+            ),
+            htmlOutput("qids"),
+            hr(),
+            radioButtons("rb",
+                         label = NULL,
                          choiceNames = list(
                              "custom",
                              "example 1 (Covid-19, SARS-CoV-2, 2019–20 COVID-19 pandemic)",
-                             "example 2 (Protein, defensin, protease, cyclic peptide)"
+                             "example 2 (antiviral drug, defensin, protease, cyclic peptide)"
                          ),
                          choiceValues = list(
-                             "custom", "example1", "example2"
+                             "custom",
+                             "example1",
+                             "example2"
                          )),
             actionButton("button.view", "Calculate network"),
+            hr(),
             sliderInput("charge",
                         "node separation",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30),
+            hr(),
+            helpText(p("Code for this page can be found at",
+                       a(href="https://github.com/TS404/TopicNetwork","github.com/TS404/TopicNetwork"),
+            )),
         ),
         # Show the network
         mainPanel(
-            forceNetworkOutput("force"),
-            renderText("query")
+            forceNetworkOutput("force", height = "800px"),
+            # chorddiagOutput("chord", height = "800px")
         )
     )
 ))
